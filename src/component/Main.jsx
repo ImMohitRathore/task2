d7297de2ccd1bab3f5f3e1bdd4ebe83b1a252bde
mohit rathore <hackside715@gmail.com>
2021-09-17 13:37:05 -0400

    make some changes in Main.js file

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,52 +1,39 @@
-import React,{ useEffect, useRef ,useState} from 'react'
+import userEvent from '@testing-library/user-event'
+import React, { useEffect, useState } from 'react'
+
 import InfiniteScroll from 'react-infinite-scroll-component';
+
 const Main = () => {
-    const [data, setData] = useState<any>({});
-    useEffect(() => {
-      fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setData(data);
-        });
-    }, []);
+    const [ input , setinput] = useState([])
+    const [page , setpage] = useState(1)
+    const datacheck = async ()=>{
+            const res = await fetch("https://jsonplaceholder.typicode.com/photos")
+           setinput( await res.json())
+           console.log(input);
+
+            
+           
+    }
+    useEffect(()=>{
+        datacheck()
+    },[])
     return (
-      <div style={{ height: "600px" }}>
-        <img src={data.url} alt="pic" />
-      </div>
-    );
-  }
-  
-  const pageSize = 5;
-  export default function App() {
-    const [count, setCount] = useState(0);
-    const ref = useRef(null);
-  
-    const isBottomVisible = useIntersectionObserver(
-      ref,
-      {
-        threshold: 0
-      },
-      false
-    );
-  
-    useEffect(() => {
-      isBottomVisible && setCount(count + 1);
-    }, [isBottomVisible]);
-  
-    return (
-      <div className="App">
-        {(() => {
-          const children = [];
-          for (let i = 1; i <= count * pageSize; i++) {
-            children.push(<Component key={i} id={i} />);
-          }
-          return children;
-        })()}
-        <div ref={ref} style={{ width: "100%", height: "20px" }}>
-          Bottom
-        </div>
-      </div>
-    );
-  }
-  
+        <>
+      
+           {input.map((ele)=>{
+              return(
+                  <>
+              
+
+
+                        
+                  <img src={ele.thumbnailUrl} alt="" />
+              
+                  </>
+              )
+           })}
+        </>
+    )
+}
+
 export default Main
